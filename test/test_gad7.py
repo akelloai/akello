252896866720059358ecd96f06f4adbdc9983061
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2023-05-04 20:42:21 -0700

    fixing unit tests

--- test/test_gad7.py
+++ test/test_gad7.py
@@ -1,10 +1,13 @@
-import unittest
+import unittest, responses
 from screening.mental_health import GAD7
-
+from settings import API_URL
 
 class TestGAD7(unittest.TestCase):
 
+    @responses.activate
     def test_gad7_test(self):
+        responses.add(responses.POST, API_URL,
+                      json={'score': 3}, status=200)
         gad7 = GAD7(akello_api_token='<token>')
         gad7.score_screener()
         assert (len(gad7.questions) == 7)
